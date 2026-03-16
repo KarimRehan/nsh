77201d83515c1968d89edb5ce0dfbba810e42fd9
karim <karim.ie.rehan@gmail.com>
2026-03-17 00:16:49 +0200

    Second commit

--- src/executer.cpp
+++ src/executer.cpp
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include "executer.hpp"
 #include "builtins.hpp"
 #include "unistd.h"
@@ -7,12 +8,24 @@
 
 void Executer::execute(const std::vector<std::string> &tokens)
 {
-    if (Builtins::handle(tokens))
-        return;
+    if (Builtins::handle(tokens)) {return;}
+
+	std::vector<std::string> args = tokens;
+	std::string outputFile;
+
+	for (size_t i = 0; i < args.size(); i++)
+	{
+    		if (args[i] == ">")
+    		{
+       	 		outputFile = args[i + 1];
+       	 		args.resize(i);
+       	 		break;
+   	 	}
+	}
 
     std::vector<const char *> argv;
 
-    for (const std::string &token : tokens)
+    for (const std::string &token : args)
         argv.push_back(token.c_str());
     argv.push_back(nullptr);
 
@@ -22,6 +35,12 @@ void Executer::execute(const std::vector<std::string> &tokens)
         std::cerr << tokens[0] << ": failed to execute command" << std::endl;
     else if (pid == 0) // child process
     {
+	if (!outputFile.empty())
+	{
+    		int fd = open(outputFile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    		dup2(fd, STDOUT_FILENO);
+    		close(fd);
+	}
         int status = execvp(argv[0], const_cast<char *const *>(argv.data()));
 
         if (status != 0)
